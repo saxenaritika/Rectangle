b9e6fbf4e38dfb492b1b60f488e247749a19caaa
Ritika Saxena <ritikasaxena3007@gmail.com>
2022-07-28 12:12:45 +0000

    Initial commit

---
+++ src/main/java/com/thoughtworks/rectangle/Rectangle.java
@@ -0,0 +1,18 @@
+
+package com.thoughtworks.rectangle;
+
+public class Rectangle{
+
+    private final int length,breadth;
+
+    Rectangle(int length, int breadth)
+    {
+        this.length=length;
+        this.breadth=breadth;
+    }
+
+    public int area()
+    {
+        return length*breadth;
+    }
+}
