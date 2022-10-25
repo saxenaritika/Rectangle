c0624fa41d70d98f2879b67af9e6e06d97bb7e95
Ritika Saxena <ritika.saxena@thoughtworks.com>
2022-10-25 12:03:57 +0530

    Refactor. area

--- src/test/java/com/thoughtworks/rectangle/RectangleTest.java
+++ src/test/java/com/thoughtworks/rectangle/RectangleTest.java
@@ -2,15 +2,16 @@ package com.thoughtworks.rectangle;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 class RectangleTest {
 
     @Test
-    void shouldReturnWhenLengthAndBreadthArePositivee() {
+    void shouldReturnAreaWhenLengthAndBreadthArePositivee() {
         Rectangle rectangle = new Rectangle(10, 10);
         int area = rectangle.area();
-        assertThat(area,is(equalTo(100)));
+        assertThat(area, is(equalTo(100)));
     }
 }
