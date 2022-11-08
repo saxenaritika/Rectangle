52fb4e3dcf86ce8aa898bbf5c97052cedc68eb0d
Ritika Saxena <ritikasaxena3007@gmail.com>
2022-11-08 10:39:51 +0000

    Refactor. area method name

--- src/test/java/com/thoughtworks/rectangle/RectangleTest.java
+++ src/test/java/com/thoughtworks/rectangle/RectangleTest.java
@@ -9,9 +9,9 @@ import org.junit.jupiter.api.Test;
 class RectangleTest {
 
     @Test
-    void shouldReturnAreaWhenLengthAndBreadthArePositive() {
+    void shouldReturnWhenAreaLengthAndBreadthArePositive() {
         Rectangle rectangle = new Rectangle(10, 10);
         int area = rectangle.area();
-        assertEquals(100, area);;;;
+        assertEquals(100, area);
     }
 }
